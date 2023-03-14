7775e9f940daa1b51aee135e3e2dd3661a518f56
Nurkanat <nurkanat.doszhan@gmail.com>
2023-03-15 01:34:23 +0600

    допилить добавление карточки

--- src/BoardPage/BoardPage.js
+++ src/BoardPage/BoardPage.js
@@ -12,34 +12,6 @@ const BoardPage = (props) => {
     const loc = JSON.parse(localStorage.getItem(props.id))
     // console.log(loc)
   })
-  
-  const AddNewCard = () => {
-    if(!addListBtn) {
-      return (
-        <button className='btn btn-primary text-center' style={{ width: '260px' }}
-          onClick={() => {setAddListBtn(true)}}>
-          <i className="bi-plus fs-6"/> Создать список
-        </button>
-      )
-    } else {
-      return (
-        <div className="d-flex flex-column align-items-start
-          p-1 bg-light border border-success border-opacity-50 rounded">
-          <input required autoFocus onChange={e => setInpVal(e.target.value)}
-            className="form-control" defaultValue={inpVal} type='text' style={{ width: '260px' }} />
-          <div className="mt-2 w-100 d-flex align-items-start">
-            <button className="btn btn-primary btn-md me-2" onClick={() => {
-              addList();
-              setAddListBtn(false);
-              setInpVal('')}}>Добавить список</button>
-            <span onClick={() => {setAddListBtn(false); setInpVal('')}} style={{ cursor: 'pointer' }}>
-              <i className="bi-x fs-4 text-dark" />
-            </span>
-          </div>
-        </div>
-      )
-    }
-  }
 
   const addList = () => {
     const data = JSON.parse(localStorage.getItem(props.id))
@@ -60,17 +32,48 @@ const BoardPage = (props) => {
     n = new Array;
     n.join(newList)
     // data.card = newList;
-    for(let i=0; i<data.card.length; i++) {
-      console.log(i)
-    }
-    console.log(data)
+    // console.log(n)
+    // console.log(newList)
+    // for(let i=0; i<data.card.length; i++) {
+    //   console.log(i)
+    // }
+    // console.log(data)
     // localStorage.setItem(data.id, JSON.stringify(data))
+  }
+  
+  const AddNewCard = () => {
+    if(!addListBtn) {
+      return (
+        <button className='btn btn-primary text-center' style={{ width: '200px' }}
+          onClick={() => {setAddListBtn(true)}}>
+          <i className="bi-plus fs-6"/> Создать список
+        </button>
+      )
+    } else {
+      return (
+        <div className="d-flex flex-column align-items-start
+          p-1 bg-light border border-success border-opacity-50 rounded">
+          <input required autoFocus onChange={e => setInpVal(e.target.value)}
+            className="form-control" defaultValue={inpVal} type='text' style={{ width: '200px' }} />
+          <div className="mt-2 w-100 d-flex align-items-start">
+            <button className="btn btn-primary btn-md me-2" onClick={() => {
+              addList();
+              setAddListBtn(false);
+              setInpVal('')}}>Добавить список</button>
+            <span onClick={() => {setAddListBtn(false); setInpVal('')}} style={{ cursor: 'pointer' }}>
+              <i className="bi-x fs-4 text-dark" />
+            </span>
+          </div>
+        </div>
+      )
+    }
   }
 
   const Card = (props) => {
+    console.log(props)
     return (
       <div className="d-flex flex-column align-items-start p-1 bg-light border border-success border-opacity-50 rounded">
-        <h3 className="text-dark">{props.title}</h3>
+        <h3 className="text-dark">{list.id}</h3>
         <textarea required autoFocus className="form-control" type='text' style={{ width: '200px' }} />
       </div>
     )
@@ -82,7 +85,7 @@ const BoardPage = (props) => {
         <h2>{props.title}</h2>
         <small>{props.createdDate.slice(0, 10)}</small>
       </div>
-      <div className='container d-flex align-items-start'>
+      <div className='container d-flex align-items-start mt-2 flex-wrap'>
         {
           list.map((i, v) => {
             return (
